18589351c2be99931cc6ac3e597e4c5f0d9951e9
mboyd1 <michael.boyd@gmail.com>
2021-01-06 20:52:21 -0600

    charlie lee removal

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,11 +7,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/litecoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/litecoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/doriancoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/doriancoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/doriancoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/doriancoin-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/doriancoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 

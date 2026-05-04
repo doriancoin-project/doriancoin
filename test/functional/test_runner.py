0e70f63f1c0d385554f3deec4908926ad1b1e0f0
David Burkett <davidburkett38@gmail.com>
2026-05-04 16:19:40 -0400

    net: Increase MAX_PROTOCOL_MESSAGE_LENGTH to 32MB
    (cherry picked from commit 457bcd78caa6d877acd3abaa3a4ab2ec9bc59cc0)

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -253,6 +253,7 @@ BASE_SCRIPTS = [
     'mweb_p2p.py',
     'mweb_p2p_mutated_compactblock.py',
     'mweb_p2p_mutated_block_submitblock.py',
+    'mweb_pre_activation_hogex_marker.py',
     'mweb_pegout_all.py',
     'mweb_node_compatibility.py',
     'mweb_wallet_address.py',
